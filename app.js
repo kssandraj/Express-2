37a4028c942c70c30b8d848d72a05866428af22b
Cassandra Juif <cassandra.juif@gmail.com>
2022-11-10 17:16:22 +0100

    adding validators.js

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
-const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
-};
+app.use(express.json());
 
-app.get("/", welcome);
+const { validateMovie } = require("./validators.js");
+const { validateUser } = require("./validators.js");
 
 const movieHandlers = require("./movieHandlers");
+const usersHandlers = require("./usersHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+/*------------- USER PART ---------------*/
+
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUsersById);
+
+app.post("/api/users", validateUser, usersHandlers.postUser);
+
+app.put("/api/users/:id", validateUser, usersHandlers.updateUser);
+
+app.delete("/api/users/:id", usersHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
