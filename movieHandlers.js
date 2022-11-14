2b8aa30f002a9377a4badf24df5d8390b7484236
Cassandra Juif <cassandra.juif@gmail.com>
2022-11-14 19:03:34 +0100

    Adding get details.

--- movieHandlers.js
+++ movieHandlers.js
@@ -48,17 +48,29 @@ const getMovieById = (req, res) => {
 };
 
 const getMovies = (req, res) => {
+  let sql = "select * from movies";
+  const sqlValues = [];
+
+  if (req.query.color != null) {
+    sql += " where color = ?";
+    sqlValues.push(req.query.color);
+
+    if (req.query.max_duration != null) {
+      sql += " where duration <= ?";
+      sqlValues.push(req.query.max_duration);
+    }
+  } else if (req.query.max_duration != null) {
+    sql += " where duration <= ?";
+    sqlValues.push(req.query.max_duration);
+  }
+
   database
-
-    .query("select * from movies")
-
+    .query(sql, sqlValues)
     .then(([movies]) => {
       res.json(movies);
     })
-
     .catch((err) => {
       console.error(err);
-
       res.status(500).send("Error retrieving data from database");
     });
 };
